90b252b2e072fe0594807009206366bb9b014b2c
asenprodanov <asenprodanov@gmail.com>
2014-07-11 00:53:56 +0300

    second commit

--- gruntfile.js
+++ gruntfile.js
@@ -1,8 +1,38 @@
 module.exports = function(grunt) {
     grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json')
-
+        pkg: grunt.file.readJSON('package.json'),
+        autoprefixer: {
+            css: {
+                files: {
+                    'app/css/main.css':'app/css/main.css'
+                }
+            }
+        },
+        concat:{   
+		    css: {
+		        src: ['app/css/normalize.css','app/css/main.css'],
+		        dest:'dist/app/css/main.css',
+		    }
+		},
+		cssmin: {
+		    css: {
+		        src:'dist/app/css/main.css',
+		        dest:'dist/app/css/main.min.css'
+		    }
+		},
+		watch: {
+		    css: {
+                files: ['app/css/*.css'],
+                tasks: ['autoprefixer','concat:css','cssmin:css'],
+                options: {
+                    spawn: false
+                }
+            }
+		}
     });
-    
-    grunt.registerTask('default');
+    grunt.loadNpmTasks('grunt-autoprefixer');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.registerTask('default',['concat','cssmin','watch']);
 };
